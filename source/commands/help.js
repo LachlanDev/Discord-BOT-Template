e545d5b954f7cbd9faeeae47ad428a3cb1a5e67a
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2021-03-11 11:44:27 +1100

    Update help.js

--- source/commands/help.js
+++ source/commands/help.js
@@ -4,8 +4,8 @@ exports.run = (client, message, args) =>{
     const help = new discord.MessageEmbed()
     .setColor('#b434eb')
     .setTitle('BOT Template')
-    .setURL("https://github.com/PapaSnags/Discord-BOT-Template")
+    .setURL("https://github.com/LachlanDev/Discord-BOT-Template")
     .addField("Info", "This is a template for Discord BOT's.")
-    .setFooter("BOT Template Made by PapaSnags#1555", "https://avatars1.githubusercontent.com/u/58458169?s=460&u=79564adeae9287fecf24814f64ed89cff91ca358&v=4")
+    .setFooter("BOT Template Made by LachlanDev#8014", "https://avatars1.githubusercontent.com/u/58458169?s=460&u=79564adeae9287fecf24814f64ed89cff91ca358&v=4")
     message.channel.send({embed: help })
 };
